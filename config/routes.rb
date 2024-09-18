d0dbfac4189d0e467d352bc8ad3dda9359199ba1
Mmanare Mahlatse nkoana <mahlatsemmanarenkoana@gmail.com>
2024-09-18 17:05:43 +0200

    rails yelp

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,14 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "restaurants", to: "restaurants#index"
-  get "restaurants/:id", to: "restaurants#show", as: :restaurant
   get "restaurants/new", to: "restaurants#new"
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  post "restaurants", to: "restaurants#create", as: :create_restaurant
+  get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  patch "restaurants/:id", to: "restaurants#update", as: :update_restaurant
+  delete "restaurants/:id", to: "restaurants#destroy", as: :destroy_restaurant
+  get "/restaurants/:id/reviews/new", to: "reviews#new", as: :new_review
+  post "/restaurants/:id/reviews", to: "reviews#create", as: :create_review
+  delete "/reviews/:id", to: "reviews#destroy", as: :destroy_review
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_18_094435) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_18_143305) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "restaurant_id", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end
